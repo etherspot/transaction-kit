f26bbb5515156dd65afaae789d6fbfe3cb61a800
Ardian <34317628+arddluma@users.noreply.github.com>
2023-01-30 12:59:17 +0100

    Implement npm publish pipeline

---
+++ .circleci/announcePublish.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+
+applicationName=$1
+package=$2
+
+payload=$(
+cat <<EOM
+{
+    "attachments": [
+        {
+            "fallback": "A new version of $applicationName is available for use. :circleci-pass:",
+            "color": "#33CC66",
+            "pretext": "A new version of $applicationName is available for use. :circleci-pass:",
+            "title": "$CIRCLE_PROJECT_REPONAME",
+            "title_link": "https://github.com/etherspot/react-etherspot-ui-buidler/releases/tag/$package",
+            "text": "Version: @etherspot/ui@$package",
+            "ts": $(date '+%s')
+        }
+    ]
+}
+EOM
+)
+
+curl -X POST --data-urlencode payload="$payload" "$SLACK_WEBHOOK_URL"
