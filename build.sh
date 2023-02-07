fcf58bd2f23e78d45f5e830f62e179d6a6bf8089
Ardian <34317628+arddluma@users.noreply.github.com>
2023-02-07 18:52:19 +0100

    Add postinstall script

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 # !/bin/bash
-npm install && npm run rollup:build
 cd example && sed -i.bak "s#process.env.REACT_APP_CHAIN_ID#\"$CHAIN_ID\"#g" src/App.tsx && sed -i.bak "s#process.env.REACT_APP_CHAIN_ID#\"$CHAIN_ID\"#g" src/index.tsx && sed -i.bak "s#process.env.REACT_APP_DEMO_WALLET_PK#\"$DEMO_WALLET_PK\"#g" src/index.tsx && npm install && npm run build 
