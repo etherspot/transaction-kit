559b308e5b65cab78db63add7928b1e6938f643f
Ardian <34317628+arddluma@users.noreply.github.com>
2023-02-07 17:11:17 +0100

    Add sed to replace env vars

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 npm install && npm run rollup:build
-cd example && echo "REACT_APP_CHAIN_ID=$CHAIN_ID" >> .env && echo "REACT_APP_DEMO_WALLET_PK=$DEMO_WALLET_PK" >> .env && echo "--------env" && cat .env && npm install && npm run build 
+cd example && sed -i.bak "s#process.env.REACT_APP_CHAIN_ID#\"$CHAIN_ID\"#g" src/App.tsx && sed -i.bak "s#process.env.REACT_APP_CHAIN_ID#\"$CHAIN_ID\"#g" src/index.tsx && sed -i.bak "s#process.env.REACT_APP_DEMO_WALLET_PK#\"$DEMO_WALLET_PK\"#g" src/index.tsx && npm install && npm run build 
