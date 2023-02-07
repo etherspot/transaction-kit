df3ac6cd33b976e25fd3be7726b1b6ddcd08d032
Ardian <34317628+arddluma@users.noreply.github.com>
2023-02-07 16:30:27 +0100

    Add env vars to build.sh / change order

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 npm install && npm run rollup:build
-cd example && echo "REACT_APP_CHAIN_ID=$REACT_APP_CHAIN_ID" >> .env && echo "REACT_APP_DEMO_WALLET_PK=$REACT_APP_DEMO_WALLET_PK" >> .env && npm install && npm run build 
+cd example && echo "REACT_APP_CHAIN_ID=$REACT_APP_CHAIN_ID" >> .env && echo "REACT_APP_DEMO_WALLET_PK=$REACT_APP_DEMO_WALLET_PK" >> .env && echo "--------env" && cat .env && npm install && npm run build 
