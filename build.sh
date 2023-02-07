a9b7cbf13eebbef201996bb194be83028c156dcd
Ardian <34317628+arddluma@users.noreply.github.com>
2023-02-07 16:00:37 +0100

    Add env vars to build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 npm install && npm run rollup:build
-cd example && npm install && npm run build
+cd example && npm install && npm run build && echo "REACT_APP_CHAIN_ID=$REACT_APP_CHAIN_ID" >> .env && echo "REACT_APP_DEMO_WALLET_PK=$REACT_APP_DEMO_WALLET_PK" >> .env
