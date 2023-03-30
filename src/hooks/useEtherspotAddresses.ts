e101bb107a4487c48bf65b09058171ed44e9026a
Kieran Goodary <kgoodary@gmail.com>
2023-03-30 21:38:22 +0100

    Reworked addresses hook, bumped package

--- src/hooks/useEtherspotAddresses.ts
+++ src/hooks/useEtherspotAddresses.ts
@@ -15,47 +15,86 @@ const useEtherspotAddresses = (): (ISmartWalletAddress | null)[] => {
     let shouldUpdate = true;
 
     const updateEtherspotAddresses = async () => {
+      const computedContractAddress: (ISmartWalletAddress | null)[] = [];
       if (!sdk) {
         return null;
       }
 
-      const computedContractAddress = await Promise.all(
-        sdk.supportedNetworks
-        .map(async (supportedNetwork) => {
-          const sdkForChainId = getSdkForChainId(supportedNetwork.chainId);
+      for (let index = 0; index < sdk.supportedNetworks.length; index++) {
+        const supportedNetwork = sdk.supportedNetworks[index];
+
+        const sdkForChainId = getSdkForChainId(supportedNetwork.chainId);
   
-          if (sdkForChainId) {
-            if (sdkForChainId?.state?.account?.type !== AccountTypes.Contract) {
-              const isConnecting = connect(supportedNetwork.chainId);
-        
-              await isConnecting;
-            }
-
-            /**
-             * SLEEP!
-             */
-            await sleep(2);
-
-            const response = await sdkForChainId.computeContractAccount({
-              sync: false
-            }).catch((e) => {
-              console.warn('An error occured whilst trying to compute contract account:', e);
-              return e;
-            });
-
-            const accountData: ISmartWalletAddress =  {
-              chainId: supportedNetwork.chainId,
-              address: response.address, 
-              chainName: supportedNetwork.name,
-            };
-
-            return accountData;
-          } else {
-            console.warn(`Sorry, the SDK is not ready yet for chain ${supportedNetwork.name} - please try again.`);
-            return null;
+        if (sdkForChainId) {
+          if (sdkForChainId?.state?.account?.type !== AccountTypes.Contract) {
+            const isConnecting = connect(supportedNetwork.chainId);
+      
+            await isConnecting;
           }
-        })
-      );
+
+          /**
+           * SLEEP!
+           */
+          await sleep(2);
+
+          const response = await sdkForChainId.computeContractAccount({
+            sync: false
+          }).catch((e) => {
+            console.warn('An error occured whilst trying to compute contract account:', e);
+            return e;
+          });
+
+          const accountData: ISmartWalletAddress =  {
+            chainId: supportedNetwork.chainId,
+            address: response.address, 
+            chainName: supportedNetwork.name,
+          };
+
+          computedContractAddress.push(accountData);
+        } else {
+          console.warn(`Sorry, the SDK is not ready yet for chain ${supportedNetwork.name} - please try again.`);
+          computedContractAddress.push(null);
+        }
+        
+      }
+
+      // const computedContractAddress = await Promise.all(
+      //   sdk.supportedNetworks
+      //   .map(async (supportedNetwork) => {
+      //     const sdkForChainId = getSdkForChainId(supportedNetwork.chainId);
+  
+      //     if (sdkForChainId) {
+      //       if (sdkForChainId?.state?.account?.type !== AccountTypes.Contract) {
+      //         const isConnecting = connect(supportedNetwork.chainId);
+        
+      //         await isConnecting;
+      //       }
+
+      //       /**
+      //        * SLEEP!
+      //        */
+      //       await sleep(2);
+
+      //       const response = await sdkForChainId.computeContractAccount({
+      //         sync: false
+      //       }).catch((e) => {
+      //         console.warn('An error occured whilst trying to compute contract account:', e);
+      //         return e;
+      //       });
+
+      //       const accountData: ISmartWalletAddress =  {
+      //         chainId: supportedNetwork.chainId,
+      //         address: response.address, 
+      //         chainName: supportedNetwork.name,
+      //       };
+
+      //       return accountData;
+      //     } else {
+      //       console.warn(`Sorry, the SDK is not ready yet for chain ${supportedNetwork.name} - please try again.`);
+      //       return null;
+      //     }
+      //   })
+      // );
   
       // Finally, set the Etherspot addresses.
       if (shouldUpdate) {
