90c88a0afce62a7f57608272b2e58edfe07323e7
poocart <7067483+poocart@users.noreply.github.com>
2023-06-06 18:26:49 +0300

    added render test for etherspot-prime

--- __tests__/hooks/useEtherspotTransactions.test.js
+++ __tests__/hooks/useEtherspotTransactions.test.js
@@ -22,36 +22,45 @@ const TestSingleBatchComponent = () => (
 );
 
 describe('useEtherspotTransactions()', () => {
-  it('returns grouped batches', () => {
+  it('returns grouped batches', async () => {
     const wrapper = ({ children }) => (
       <EtherspotTransactionKit provider={null}>
         <div>
           test
           <span>
-          <EtherspotBatches>
-            <EtherspotBatch chainId={123} gasTokenAddress={'testGasTokenAddress'}>
-              <EtherspotTransaction
-                to={'0x12'}
-                data={'0x0'}
-                value={'0.123'}
-              />
-              <EtherspotTransaction
-                to={'0x0'}
-                data={'0xFFF'}
-                value={'420'}
-              />
-            <EtherspotContractTransaction
-              abi={['function transfer(address, uint)']}
-              contractAddress={'0xe3818504c1b32bf1557b16c238b2e01fd3149c17'}
-              methodName={'transfer'}
-              params={['0x7F30B1960D5556929B03a0339814fE903c55a347', ethers.utils.parseEther('123')]}
-            />
-            </EtherspotBatch>
-          </EtherspotBatches>
-        </span>
+            <EtherspotBatches>
+              <EtherspotBatch chainId={123} gasTokenAddress={'testGasTokenAddress'}>
+                <EtherspotTransaction
+                  to={'0x12'}
+                  data={'0x0'}
+                  value={'0.123'}
+                />
+                <EtherspotTransaction
+                  to={'0x0'}
+                  data={'0xFFF'}
+                  value={'420'}
+                />
+                <EtherspotContractTransaction
+                  abi={['function transfer(address, uint)']}
+                  contractAddress={'0xe3818504c1b32bf1557b16c238b2e01fd3149c17'}
+                  methodName={'transfer'}
+                  params={['0x7F30B1960D5556929B03a0339814fE903c55a347', ethers.utils.parseEther('123')]}
+                />
+              </EtherspotBatch>
+            </EtherspotBatches>
+          </span>
         </div>
         <EtherspotBatches skip>
           <span>test</span>
+        </EtherspotBatches>
+        <EtherspotBatches via={'etherspot-prime'}>
+          <EtherspotBatch chainId={69}>
+            <EtherspotTransaction
+              to={'0x420'}
+              data={'0x69420'}
+              value={'69'}
+            />
+          </EtherspotBatch>
         </EtherspotBatches>
         <EtherspotBatches>
           <span>test</span>
@@ -63,18 +72,24 @@ describe('useEtherspotTransactions()', () => {
 
     const { result: { current } } = renderHook(() => useEtherspotTransactions(), { wrapper });
 
-    expect(current.batches.length).toBe(4);
+    expect(current.batches.length).toBe(5);
     expect(current.batches[0].batches.length).toBe(1);
     expect(current.batches[0].batches[0].chainId).toBe(123);
     expect(current.batches[0].batches[0].gasTokenAddress).toBe('testGasTokenAddress');
     expect(current.batches[0].batches[0].transactions.length).toBe(3);
     expect(current.batches[0].batches[0].transactions[1].to).toBe('0x0');
     expect(current.batches[0].batches[0].transactions[1].data).toBe('0xFFF');
-    expect(current.batches[0].batches[0].transactions[1].value.toJSON()).toStrictEqual({"hex": "0x16c4abbebea0100000", "type": "BigNumber"});
+    expect(current.batches[0].batches[0].transactions[1].value.toJSON()).toStrictEqual({ 'hex': '0x16c4abbebea0100000', 'type': 'BigNumber' });
     expect(current.batches[0].batches[0].transactions[2].to).toBe('0xe3818504c1b32bf1557b16c238b2e01fd3149c17');
     expect(current.batches[0].batches[0].transactions[2].data).toBe('0xa9059cbb0000000000000000000000007f30b1960d5556929b03a0339814fe903c55a347000000000000000000000000000000000000000000000006aaf7c8516d0c0000');
     expect(current.batches[0].batches[0].transactions[2].value).toBe(undefined);
     expect(current.batches[1].skip).toBe(true);
+    expect(current.batches[2].batches.length).toBe(1);
+    expect(current.batches[2].batches[0].chainId).toBe(69);
+    expect(current.batches[2].batches[0].transactions.length).toBe(1);
+    expect(current.batches[2].batches[0].transactions[0].to).toBe('0x420');
+    expect(current.batches[2].batches[0].transactions[0].data).toBe('0x69420');
+    expect(current.batches[2].batches[0].transactions[0].value.toJSON()).toStrictEqual({ 'hex': '0x03bd913e6c1df40000', 'type': 'BigNumber' });
   });
 
   it('throws an error if <EtherspotBatches /> within <EtherspotBatches />', () => {
@@ -141,7 +156,7 @@ describe('useEtherspotTransactions()', () => {
   it('throws an error if <EtherspotBatch /> rendered without <EtherspotBatches />', () => {
     expect(() => render(
       <EtherspotBatch>
-          <span>test</span>
+        <span>test</span>
       </EtherspotBatch>
     ))
       .toThrow('No parent <EtherspotBatches />');
@@ -150,7 +165,7 @@ describe('useEtherspotTransactions()', () => {
   it('throws an error if <EtherspotTransaction /> rendered without <EtherspotBatch />', () => {
     expect(() => render(
       <EtherspotTransaction to={'0x'}>
-          <span>test</span>
+        <span>test</span>
       </EtherspotTransaction>
     ))
       .toThrow('No parent <EtherspotBatch />');
