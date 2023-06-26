1dea3558e97c46be77750f8cb354804c0f4ac66d
poocart <7067483+poocart@users.noreply.github.com>
2023-06-26 19:13:46 +0300

    updated tests

--- __mocks__/@etherspot/prime-sdk.js
+++ __mocks__/@etherspot/prime-sdk.js
@@ -1,4 +1,4 @@
-import EtherspotPrime from 'etherspot-prime';
+import * as EtherspotPrime from '@etherspot/prime-sdk';
 
 export class PrimeSdk {
   constructor () {}
@@ -7,5 +7,7 @@ export class PrimeSdk {
     return '0x07ff85757f5209534EB601E1CA60d72807ECE0bC';
   }
 }
+
+export const isWalletProvider = EtherspotPrime.isWalletProvider;
 
 export default EtherspotPrime;
